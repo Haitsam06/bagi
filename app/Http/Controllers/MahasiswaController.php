b0ae736d57e988b668299a9453cba64bd3ace553
Gugugaga25 <banupandaraa@gmail.com>
2026-03-06 13:23:30 +0700

    perbaikan lanjutan

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -10,6 +10,42 @@ class MahasiswaController extends Controller
     public function index()
     {
         $mahasiswa = Mahasiswa::all();
-        return view('mahasiswa.index', compact('mahasiswa'));
+        return response()->json($mahasiswa);
+        // return view('mahasiswa.index', compact('mahasiswa'));
+    }
+
+    public function store(Request $request)
+    {
+        $mahasiswa = Mahasiswa::create($request->all());
+        return response()->json($mahasiswa, 201);
+    }
+
+    public function show($id)
+    {
+        $mahasiswa = Mahasiswa::find($id);
+        if (!$mahasiswa) {
+            return response()->json(['message' => 'Mahasiswa not found'], 404);
+        }
+        return response()->json($mahasiswa);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $mahasiswa = Mahasiswa::find($id);
+        if (!$mahasiswa) {
+            return response()->json(['message' => 'Mahasiswa not found'], 404);
+        }
+        $mahasiswa->update($request->all());
+        return response()->json($mahasiswa);
+    }
+
+    public function destroy($id)
+    {
+        $mahasiswa = Mahasiswa::find($id);
+        if (!$mahasiswa) {
+            return response()->json(['message' => 'Mahasiswa not found'], 404);
+        }
+        $mahasiswa->delete();
+        return response()->json(['message' => 'Mahasiswa deleted']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\User;
+use App\Http\Controllers\MahasiswaController;
+
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
@@ -10,3 +13,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function () {
     return User::all();
 });
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
+Route::get('/mahasiswa/{id}', [MahasiswaController::class, 'show']);
+Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
+Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy']);
